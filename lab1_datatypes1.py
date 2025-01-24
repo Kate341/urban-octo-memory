508998b5959f0703bb93ddf104faa9626159b878
Kate <romanovakata560@gmail.com>
2025-01-24 23:31:57 +0500

    data types

---
+++ lab1_datatypes1.py
@@ -0,0 +1,55 @@
+x = "Hello World"
+print(x)
+print(type(x))
+
+y = 28
+print(y)
+print(type(y))
+
+z = 1j
+print(z)
+print(type(z)) 
+
+a = ["apple", "banana", "cherry"]
+print(a)
+print(type(a))
+
+b = ("apple", "banana", "cherry")
+print(b)
+print(type(b)) 
+
+c = range(6)
+print(c)
+print(type(c)) 
+
+e = {"name" : "John", "age" : 36}
+print(e)
+print(type(e))
+
+f = {"apple", "banana", "cherry"}
+print(f)
+print(type(f)) 
+
+g = frozenset({"apple", "banana", "cherry"})
+print(g)
+print(type(g)) 
+
+i = True
+print(i)
+print(type(i))
+
+j = b"Hello"
+print(j)
+print(type(j)) 
+
+k = bytearray(5)
+print(k)
+print(type(k))
+
+l = memoryview(bytes(5))
+print(l)
+print(type(l)) 
+
+m = None
+print(m)
+print(type(m)) 
