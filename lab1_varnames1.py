c690644257d17b940e946f6a52f395ffc42ae93d
Kate <romanovakata560@gmail.com>
2025-01-24 22:19:20 +0500

    legal names

---
+++ lab1_varnames1.py
@@ -0,0 +1,14 @@
+myvar = "Kate"
+my_var = "Kate"
+_my_var = "Kate"
+myVar = "Kate"
+MYVAR = "Kate"
+myvar2 = "Kate"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
