33359841aea840961eb328455721a075ce9af107
Kate <romanovakata560@gmail.com>
2025-02-01 06:52:06 +0500

    python tuples

---
+++ lab2_pytuple.py
@@ -0,0 +1,8 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = tuple(("apple", "banana", "cherry")) 
+print(thistuple)
