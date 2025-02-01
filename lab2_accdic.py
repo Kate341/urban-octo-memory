cf321bac255a8323faced7fe850ea5051ffdccb6
Kate <romanovakata560@gmail.com>
2025-02-01 07:40:12 +0500

    accessing items

---
+++ lab2_accdic.py
@@ -0,0 +1,16 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+
+x = thisdict.get("model")
+print(x)
+
+x = thisdict.keys()
+print(x)
+
+x = thisdict.values()
+print(x)
