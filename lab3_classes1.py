d269d3831f4197d1a23b9ae1b0c6972c6c2ed3fa
Kate <romanovakata560@gmail.com>
2025-02-06 14:08:41 +0500

    classes 1

---
+++ lab3_classes1.py
@@ -0,0 +1,9 @@
+class ghg():
+    def __init__(value):
+        value.name = input()
+        value.type = 0
+    def Showstring(value):
+        print(value.name, "zxz")
+
+ty = ghg()
+ty.Showstring()
