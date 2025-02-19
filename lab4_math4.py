f03bef27d11e49db1127ca5924ea4fda5393acf8
Kate <romanovakata560@gmail.com>
2025-02-19 10:59:58 +0500

    area of parallelogram

---
+++ lab4_math4.py
@@ -0,0 +1,10 @@
+import math
+
+length = int(input())
+height = int(input())
+
+area = float(height * length)
+
+print("Length of base: ", length)
+print("Height of parallelogram:", height)
+print("Expected Output: ", area)
